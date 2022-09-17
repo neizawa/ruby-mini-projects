745d20103d04a630a3ee4bdce21e30c61cfe26be
neizawa <meeteori@gmail.com>
2022-09-17 13:12:56 +0500

    Update Caesar Cipher project's code

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,52 +1,19 @@
 def caesar_cipher(string, shift_factor)
-    return "ERROR" unless (string.is_a? String)
-    return "ERROR" unless (shift_factor.is_a? Numeric)
+    ascii_numbers = string.split("").map {|letter| letter.ord}
 
-    if shift_factor == 0
-        return string
-    end
-
-    letters_in_ascii = string.split("").map {|letter| letter.ord}
-
-    if shift_factor.positive?
-        shifted_letters_in_ascii = shift_forwards(letters_in_ascii, shift_factor)
-    elsif shift_factor.negative?
-        shifted_letters_in_ascii = shift_backwards(letters_in_ascii, shift_factor)
-    end
-
-    shifted_string = shifted_letters_in_ascii.map {|code| code.chr}.join("")
-end
-
-def shift_forwards(letters_in_ascii, shift_factor)
-    letters_in_ascii.map do |code|
-        shift_factor.times do
-            if code.chr == "Z" || code.chr == "z"
-                code -= 25
-            elsif code.between?(65, 90) || code.between?(97, 122)
-                code += 1
-            else
-                break
-            end
-        end
-  
-        code
-    end
-end
-
-def shift_backwards(letters_in_ascii, shift_factor)
-    letters_in_ascii.map do |code|
+    shifted_ascii_numbers = ascii_numbers.map do |letter|
         shift_factor.abs.times do
-            if code.chr == "A" || code.chr == "a"
-                code += 25
-            elsif code.between?(65, 90) || code.between?(97, 122)
-                code -= 1
-            else
-                break
+            if shift_factor.positive? && (letter.between?(65, 90) || letter.between?(97, 122))
+                (letter.chr == "Z" || letter.chr == "z") ? letter -= 25 : letter += 1
+            elsif shift_factor.negative? && (letter.between?(65, 90) || letter.between?(97, 122))
+                (letter.chr == "A" || letter.chr == "a") ? letter += 25 : letter -= 1
             end
         end
-  
-        code
+
+        letter
     end
+
+    shifted_string = shifted_ascii_numbers.map {|letter| letter.chr}.join("")
 end
 
 p caesar_cipher("Hello Odin! We do a little trolling..", 69)
